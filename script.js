741e94f005b7985d67cd7c3bb116f34c08618cf4
Akhirahstrong <codeclubdetroit@gmail.com>
2023-04-24 01:30:48 -0400

    Refresher

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstName = "Akhirah";
+let lastName = "Strong";
+let fullName = firstName + " " + lastName;
+let population = 496460;
+let tripplePop = population * 3;
+let javaScriptIsCool = true;
+let colors = ["Red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+console.log(fullName);
+console.log(population);
+console.log(tripplePop);
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i <= 4; i++) {
+  console.log(i);
+}
